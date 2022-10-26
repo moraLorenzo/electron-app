1db3a9685b69db67a3649a787658ae8c33722c64
Lorenzo Mora <201810219@gordoncollege.edu.ph>
2022-10-26 17:01:53 +0800

    Auto-update Desktop

--- app.js
+++ app.js
@@ -1,36 +1,109 @@
-const { app, BrowserWindow, autoUpdater, dialog } = require('electron');
-const { autoUpdate } = require('electron-updater');
+// const { app, BrowserWindow, ipcMain } = require('electron');
+// const { autoUpdater } = require('electron-updater');
 
-function createWindow() {
-  win = new BrowserWindow({
-    width: 1200, 
+// let mainWindow;
+
+// function createWindow () {
+//   mainWindow = new BrowserWindow({
+//     width: 800,
+//     height: 600,
+//     webPreferences: {
+//       nodeIntegration: true,
+//     },
+//   });
+//   mainWindow.loadFile('dist/electron-app/index.html');
+//   mainWindow.on('closed', function () {
+//     mainWindow = null;
+//   });
+
+//   mainWindow.once('ready-to-show', () => {
+//     autoUpdater.checkForUpdatesAndNotify();
+//   });
+// }
+
+// app.on('ready', () => {
+//   createWindow();
+// });
+
+// app.on('window-all-closed', function () {
+//   if (process.platform !== 'darwin') {
+//     app.quit();
+//   }
+// });
+
+// app.on('activate', function () {
+//   if (mainWindow === null) {
+//     createWindow();
+//   }
+// });
+
+// ipcMain.on('app_version', (event) => {
+//   event.sender.send('app_version', { version: app.getVersion() });
+// });
+
+// autoUpdater.on('update-available', () => {
+//   mainWindow.webContents.send('update_available');
+// });
+// autoUpdater.on('update-downloaded', () => {
+//   mainWindow.webContents.send('update_downloaded');
+// });
+const path = require("path");
+const { app, dialog, BrowserWindow } = require("electron");
+const { autoUpdater } = require("electron-updater");
+const isDev = require("electron-is-dev");
+
+const createWindow = () => {
+
+  const win = new BrowserWindow({
+    width: 1200,
     height: 800,
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: true
-    }
+    },
   });
 
-  win.loadFile('dist/electron-app/index.html');
-  
-  autoUpdater.checkForUpdates();
-}
+  // win.loadURL(
+  //   isDev ? "http://localhost:4200" : `file://${path.join(__dirname, "dist/electron-app/index.html")}`
+  // );
 
-app.whenReady().then(() => {
-  createWindow()
+  win.loadFile(path.join(__dirname, 'dist/electron-app/index.html'))
+
+  if(isDev){
+    win.webContents.openDevTools({mode: "detach"});
+    // require()
+  };
+  
+  if(!isDev){
+    autoUpdater.checkForUpdates();
+  };
+};
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if(process.platform !== "darwin"){
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if(BrowserWindow.getAllWindows().length === 0){
+    createWindow();
+  }
 });
 
 autoUpdater.on("update-available", (_event, releaseNotes, releaseName) => {
-  const dialogOpts = {
-    type: 'info',
-    buttons: ['Ok'],
-    title: 'Application Update',
-    message: process.platform === 'win32' ? releaseNotes : releaseName,
-    detail: 'A new version is being downloaded.'
-  }
-  dialog.showMessageBox(dialogOpts, (response) => {
+	const dialogOpts = {
+		type: 'info',
+		buttons: ['Ok'],
+		title: 'Application Update',
+		message: process.platform === 'win32' ? releaseNotes : releaseName,
+		detail: 'A new version is being downloaded. Please do not close the system.'
+	}
+	dialog.showMessageBox(dialogOpts, (response) => {
 
-  });
+	});
 })
 
 autoUpdater.on("update-downloaded", (_event, releaseNotes, releaseName) => {
@@ -39,9 +112,11 @@ autoUpdater.on("update-downloaded", (_event, releaseNotes, releaseName) => {
     buttons: ['Restart', 'Later'],
     title: 'Application Update',
     message: process.platform === 'win32' ? releaseNotes : releaseName,
-    detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+    detail: 'A new version has been installed. Restart the application to apply the updates.'
   }
-  dialog.showMessageBox(dialogOpts).then((returnValue) => {
-    if(returnValue.response === 0) autoUpdater.quitAndInstall()
-  });
-})
+  dialog.showMessageBox(dialogOpts, (returnValue) => {
+    if(returnValue.response === 0) autoUpdater.quitAndInstall();
+  })
+
+});
+
